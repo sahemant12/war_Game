0a3efa150cec858cbb7dc773dbb53f6f440685bf
Himanshu <hemantsah2912@gmail.com>
2023-07-15 15:56:43 +0530

    part-2 Done

--- script.js
+++ script.js
@@ -1,51 +1,47 @@
-const hero = {
-    elementId: "hero",
-    name: "Wizard",
-    avatar: "images/wizard.png",
-    health: 60,
-    diceRoll: [6,1,2],
-    diceCount:3
+const characterData = {
+
+     hero: {
+        elementId: "hero",
+        name: "Wizard",
+        avatar: "images/wizard.png",
+        health: 60,
+        diceCount:3
+    },   
+     monster: {
+        elementId: "monster",
+        name: "Orc",
+        avatar: "images/orc.png",
+        health: 10,
+        diceCount:1
+    }
 }
 
-const monster = {
-    elementId: "monster",
-    name: "Orc",
-    avatar: "images/orc.png",
-    health: 10,
-    diceRoll: [4],
-    diceCount:1
-}
+function Character(data){
+    Object.assign(this,data)
+    this.generateRandomNumbers = function(num){
+        const numbers = new Array(num).fill(0).map(()=>{
+            return Math.floor(Math.random() * 6) + 1
+        })
+        return numbers
+      }
 
-//random num generator b/w 1 & 6
-function generateRandomNumbers(num) {
-    // let numbers = []; 
-    // for (let i = 0; i < num; i++) {
-    //   const randomNum = Math.floor(Math.random() * 6) + 1;
-    //   numbers.push(randomNum);
-    // } 
-    // return numbers;
+      this.renderCharacter = function(){     
+        const {elementId, name, avatar, health, diceCount} = this
     
-    //better way
-    const numbers = new Array(num).fill(0).map(()=>{
-        return Math.floor(Math.random() * 6) + 1
-    })
-    return numbers
-  }
-  function renderCharacter(char){     
-
-    const {elementId, name, avatar, health, diceCount} = char
-
-    const dicevalue = generateRandomNumbers(diceCount).map((ele)=>{
-        return `<div class="dice">${ele}</div>`
-    }).join('')
-
-    let htmlCharacter = `<div class="character-card">
-    <h4 class="name"> ${name} </h4>
-    <img class="avatar" src="${avatar}"/>
-    <p class="health">health: <b> ${health} </b></p>
-    <div class="dice-container">${dicevalue}</div> `
-    document.getElementById(elementId).innerHTML = htmlCharacter;
-
+        const dicevalue = this.generateRandomNumbers(diceCount).map((ele)=>{
+            return `<div class="dice">${ele}</div>`
+        }).join('')
+    
+        return `<div class="character-card">
+        <h4 class="name"> ${name} </h4>
+        <img class="avatar" src="${avatar}"/>
+        <p class="health">health: <b> ${health} </b></p>
+        <div class="dice-container">${dicevalue}</div> `
+    
+    }
 }
-renderCharacter(hero)
-renderCharacter(monster)
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
+console.log(wizard)
+document.getElementById(wizard.elementId).innerHTML = wizard.renderCharacter()
+document.getElementById(orc.elementId).innerHTML = orc.renderCharacter()
